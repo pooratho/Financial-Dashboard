9e4754d2a6219688db9bd4c0edf47944dc939646
Parnian Taheri <prniantahry@gmail.com>
2026-08-03 17:12:10 +0330

    Initialize React frontend with Vite

---
+++ app/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+// src/App.jsx
+
+function App() {
+  // کدهای جاوا اسکریپت (مثل متغیرها و توابع) اینجا نوشته میشن
+
+  return (
+    // کدهای HTML (رابط کاربری) اینجا نوشته میشن
+    <div className="main-container">
+      <h1>داشبورد سیستم حسابداری</h1>
+      <p>طراحی شده با ری‌اکت</p>
+      <button className="btn-primary">ثبت تراکنش جدید</button>
+    </div>
+  );
+}
+
+export default App;
